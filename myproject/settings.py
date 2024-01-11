2526d20b143e4bab74c6e000dc8e77e9b4010276
Pengju He <86140794+GenjiM1n4moto@users.noreply.github.com>
2024-01-12 03:18:05 +0900

    Add mysql related settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 
